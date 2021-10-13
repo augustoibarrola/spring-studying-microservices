bb97b5946faab3bc8403586f5d81e10c3e9cd104
augustoibarrola <augusto.t.ibarrola@gmail.com>
2021-10-13 15:54:04 -0400

    test

---
+++ hotlinebling-calldetails/src/main/java/ms/hotlinebling/calldetails/HotlineblingCalldetailsApplication.java
@@ -0,0 +1,13 @@
+package ms.hotlinebling.calldetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotlineblingCalldetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotlineblingCalldetailsApplication.class, args);
+	}
+
+}
---
+++ hotlinebling-calldetails/src/test/java/ms/hotlinebling/calldetails/HotlineblingCalldetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package ms.hotlinebling.calldetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotlineblingCalldetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
