4cec3a231bacbb60b277e709fd71a28b0236232c
augustoibarrola <augusto.t.ibarrola@gmail.com>
2021-10-17 16:47:56 -0400

    eureka server started

--- hotlinebling-discovery/src/main/java/ms/hotlinebling/hotlineblingdiscovery/HotlineblingDiscoveryApplication.java
+++ hotlinebling-discovery/src/main/java/ms/hotlinebling/hotlineblingdiscovery/HotlineblingDiscoveryApplication.java
@@ -2,8 +2,10 @@ package ms.hotlinebling.hotlineblingdiscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class HotlineblingDiscoveryApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +13,8 @@ public class HotlineblingDiscoveryApplication {
 	}
 
 }
+
+/*
+ * @EnableEurekaServer
+ * - marks the application as a Eureka Server 
+ * */
