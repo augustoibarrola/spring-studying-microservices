366324dd8deb46a8f2cb59a1e439f51959a8978f
augustoibarrola <augusto.t.ibarrola@gmail.com>
2021-10-21 15:23:39 -0400

    implementing centralized remote config files for MSs

---
+++ hotlinebling-config/src/test/java/ms/hotlinebling/config/HotlineblingConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package ms.hotlinebling.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotlineblingConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
