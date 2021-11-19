44e8ae26e5c0bbdda02a2c81e17e3fc79420b373
augustoibarrola <augusto.t.ibarrola@gmail.com>
2021-11-19 14:58:45 -0500

    updated customer schema

--- hotlinebling-customer/src/main/resources/hb-customer.sql
+++ hotlinebling-customer/src/main/resources/hb-customer.sql
@@ -3,21 +3,25 @@ CREATE DATABASE hotlinebling_customer;
 USE hotlinebling_customer;
 
 CREATE TABLE Customer(
-	customer_id INTEGER PRIMARY KEY AUTO_INCREMENT,
-	phone_number BIGINT,
-	customer_name VARCHAR(50),
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	middle_name VARCHAR(50),
 	age INTEGER,
+	phone_number BIGINT,
+	e_mail VARCHAR(50),
 	address VARCHAR(50),
 	plan_id INTEGER,
 	phone_id INTEGER,
-	password VARCHAR(50)
+	password VARCHAR(50), 
+	ssn INTEGER
 );
 
 INSERT INTO Customer
-	(phone_number, customer_name, age, plan_id, password, phone_id)
+	(first_name, age, phone_number, e_mail, plan_id, phone_id, password)
 VALUES
-(606060, 'Augusto', 25, 1, '123', 1), 
-(101010, 'Anastasia', 6, 2, '123', 2), 
-(303030, 'Ricardo', 26, 3, '123', 1), 
-(202020, 'Princess', 16, 2, '123', 4), 
-(404040, 'Hunter', 8, 1, '123', 1); 
+('Augusto', 25, 2011234567, 'augusto@email.com', 1, 1,'123'), 
+('Anastasia', 6, 2011234568,'anastasia@email.com', 2, 2, '123'), 
+('Ricardo', 27, 2011234569,'ricardo@email.com', 3, 1, '123'), 
+('Princess', 16, 2011234570,'princess@email.com', 2, 4,'123'), 
+('Hunter', 8, 2011234571,'hunter@email.com',1, 1, '123');
