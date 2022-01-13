792eeb8773ef2f827dfe995c87420ce28114b61d
augustoibarrola <augusto.t.ibarrola@gmail.com>
2022-01-12 20:33:30 -0500

    reformatted calendar table in database

--- hotlinebling-calandar/src/main/resources/hb-calandar.sql
+++ hotlinebling-calandar/src/main/resources/hb-calandar.sql
@@ -2,13 +2,24 @@ DROP DATABASE IF EXISTS hb_calandar;
 CREATE DATABASE hb_calandar;
 USE hb_calandar;
 
+CREATE TABLE Calendar(
+	name VARCHAR(50) NOT NULL,
+	calendarOwner INTEGER,
+	CONSTRAINT Calendar_name_pk PRIMARY KEY (name)
+);
+
 CREATE TABLE Event(
-	event_id INTEGER PRIMARY KEY AUTO_INCREMENT,
-	customer_id INTEGER NOT NULL,
-	description TEXT(500), 
-	event_year YEAR, 
-	event_date DATE, 
-	event_time TIME, 
-	event_datetime DATETIME
+	id INT NOT NULL AUTO_INCREMENT,
+	eventOwner INTEGER,
+	calendar INTEGER,
+	eventLocation INTEGER,
+	eventDateTime DATETIME,
+	startDate DATE,
+	endDate DATE,
+	startTime DATE,
+	endTime DATE,
+	shortDescription VARCHAR(50),
+	fullDescription TEXT,
+	CONSTRAINT Event_id_pk PRIMARY KEY (id)
 );
 
